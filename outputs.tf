a0a1efe16ab1a4a5804cab15b34f5233f6d3b811
mehmet <mehmetdoganer84@gmail.com>
2023-03-03 21:08:46 -0600

    add

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 output "instances1" {
-    value = module.ec2_instances1.*.public_ip
+    value = module.ec2_instances1.public_ip
 }
 output "instances2" {
-    value = module.ec2_instances2.*.public_ip
+    value = module.ec2_instances2.public_ip
 }
 output "rds_endpoint" {
     value = module.rds.endpoint
