75abb7b4669581440cd5cfd952058ff9d9389bc7
mehmet <mehmetdoganer84@gmail.com>
2023-03-03 20:45:58 -0600

    add instance

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,8 @@
-output "instances" {
-    value = module.instances.*.public_ip
+output "instances1" {
+    value = module.ec2_instances1.*.public_ip
+}
+output "instances2" {
+    value = module.ec2_instances2.*.public_ip
 }
 output "rds_endpoint" {
     value = module.rds.endpoint
