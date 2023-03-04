d21e589e021d027320a9d2603c331a8e44d6937d
mehmet <mehmetdoganer84@gmail.com>
2023-03-03 21:34:08 -0600

    rds endpoint

--- modules/aws_rds/outputs.tf
+++ modules/aws_rds/outputs.tf
@@ -5,11 +5,3 @@ output "id" {
 output "endpoint" {
   value = aws_db_instance.db.endpoint
 }
-
-output "aws_instance1" {
-  value = module.ec2_instance1.public_ip
-}
-
-output "aws_instance2" {
-  value = module.ec2_instance2.public_ip
-}
